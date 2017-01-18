473e2976b5f04116721bf5be3d684c7f265799ca
JasmineGWT <jazzgwwt@gmail.com>
2017-01-19 11:23:59 +1300

    Added redux, clone, concurrently and added reducer

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
+import { createStore } from 'redux'
+
+import reducer from './reducer'
 
 render(<App name='jasmine.xyz' />, document.querySelector('main'))
 console.log('welcome to jasmine.xyz')
