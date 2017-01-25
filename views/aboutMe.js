3db4d83f04d442d73f0d2a225507aab976b44eca
JasmineGWT <jazzgwwt@gmail.com>
2017-01-25 13:57:03 +1300

    Components not rendering properly, thinking morphdom

--- views/aboutMe.js
+++ views/aboutMe.js
@@ -1,5 +1,9 @@
 var h = require('hyperscript')
 
-module.exports = function showAboutMe(){
-  return h('h2', {}, 'My name is jasmine')
+module.exports = function showAboutMe(i) {
+  if (i === true) {
+    return h('p', {}, 'My name is jasmine')
+  } else {
+    return h('h1', {}, 'About me')
+  }
 }
--- views/index.js
+++ views/index.js
@@ -2,17 +2,20 @@ var h = require('hyperscript')
 var main = document.querySelector('main')
 var morph = require('morphdom')
 var showAboutMe = require('./aboutMe')
+var aboutMeClicked = false
 module.exports = function showContence() {
 
 return h('div.contenceDiv', {},
 h('div.aboutMe', {}, h('h2', {onclick: function () {
-  var aboutMe = showAboutMe()
-  main.appendChild(aboutMe)
-}
- }, 'About Me')),
+  var aboutMeClicked = true
+  showAboutMe(aboutMeClicked)
+}}, showAboutMe(aboutMeClicked))),
 h('div.cv', {}, 'CV'),
 h('div.projects', {}, 'Projects'),
 h('div.contactMe', {}, 'Contact Me')
 )
 
 }
+
+var aboutMe = showAboutMe()
+main.appendChild(aboutMe)
